37ce699395a563836cea9b4dfd2a3b81a95d1581
steve keyen <stevekeyen@gmail.com>
2020-10-14 13:56:44 +0200

    first example

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ag-grid-experimentation
+yarn add ag-grid-community ag-grid-angular
--- grid-app/src/app/app.component.ts
+++ grid-app/src/app/app.component.ts
@@ -2,9 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `<ag-grid-angular style="width: 500px; height: 500px;" class="ag-theme-alpine"
+                [rowData]="rowData" [columnDefs]="columnDefs" >
+              </ag-grid-angular>`
 })
 export class AppComponent {
   title = 'grid-app';
+
+  columnDefs = [
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price'}
+  ];
+
+  rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 }
+  ];
 }
